82fb392a60623dc0bad75da0e1fe198370226abe
ERYTHR0CYTE <abollinger@gitlab.com>
2026-06-30 13:36:43 -0400

    Update resource.tf

--- examples/resources/tines_credential/resource.tf
+++ examples/resources/tines_credential/resource.tf
@@ -8,8 +8,8 @@ resource "tines_credential" "example_text_credential" {
 # token from your CI/CD pipeline or another Terraform resource). When the value
 # changes, Terraform updates the Tines Credential in place during apply.
 resource "tines_credential" "rotated_token" {
-  name    = "sirtmanager_gitlab-sirt_token"
+  name    = "example_token"
   mode    = "TEXT"
   team_id = 2
-  value   = var.rotated_gitlab_pat
+  value   = var.rotated_git_pat
 }
